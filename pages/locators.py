e48d5ed2464adf71e11ac85feef3c0f04bc2e397
DmitrY <diavseenko@yandex.ru>
2022-05-31 01:43:28 +0300

    class Login

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,10 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    REG_EMAIL_LINK = (By.NAME, "#registration-email")
+    REG_PASS_LINK = (By. NAME, "registration-password1")
+    REG_CONF_PASS_LINK = (By.NAME, "registration-password2")
+    LOGIN = (By.NAME, "login-username")
+    LOGIN_PASSWORD = (By.NAME, "login-password")
