ebfaf3e67d5b964af4e9d02e9658e501de3ec3f6
DmitrY <diavseenko@yandex.ru>
2022-06-13 00:29:33 +0300

    new class CartLocators

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,10 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REG_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class CartLocators():
+    LOGIN_FORM = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    ADD_MESSAGE = (By.CSS_SELECTOR, "div .alertinner strong")
+    PURCHASE_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+    CART_SUM = (By.CSS_SELECTOR, ".hidden-xs")
+    PURCHASE_NAME = (By.CSS_SELECTOR, ".product_main h1")
