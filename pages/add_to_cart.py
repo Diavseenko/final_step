a92e4185bb8664f337f2e52b96276fe258fb9240
DmitrY <diavseenko@yandex.ru>
2022-06-15 02:20:17 +0300

    added should_not_be_success_message and should_not_be_success_message

--- pages/add_to_cart.py
+++ pages/add_to_cart.py
@@ -9,17 +9,31 @@ class AddToCart(BasePage):
         add_button = self.browser.find_element(*CartLocators.LOGIN_FORM)
         add_button.click()
 
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*CartLocators.ADD_MESSAGE), \
+            "Success message is presented, but should not be"
+
     def check_add_to_cart(self):
-        add_message = self.browser.find_element(*CartLocators.ADD_MESSAGE)
+        add_message = self.browser.find_element(*CartLocators.ADD_MESSAGE_NAME)
         purchase_name = self.browser.find_element(*CartLocators.PURCHASE_NAME)
         assert add_message.text == purchase_name.text, "Check your purchase"
 
     def check_sum_in_cart(self):
         cart_sum = self.browser.find_element(*CartLocators.CART_SUM)
         purchase_prise = self.browser.find_element(*CartLocators.PURCHASE_PRICE)
-        print(cart_sum.text)
-        print(purchase_prise.text)
         assert cart_sum.text.find(purchase_prise.text) != -1, "The cart`s sum have a differ with price"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*CartLocators.ADD_MESSAGE), "The message is present"
+
+    def should_not_be_success_message(self):
+        assert self.is_disappeared(*CartLocators.ADD_MESSAGE), "No message"
+
+
+
+
+
+
 
 
 
