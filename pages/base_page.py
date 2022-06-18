a7a709f2bab12137d07bcab49553f7a74539ebbd
DmitrY <diavseenko@yandex.ru>
2022-06-19 02:08:38 +0300

    sorted

--- pages/base_page.py
+++ pages/base_page.py
@@ -3,8 +3,7 @@ from selenium.common.exceptions import NoAlertPresentException
 from selenium.webdriver.support import expected_conditions as EC
 import math
 from selenium.webdriver.support.wait import WebDriverWait
-from locators import BasePageLocators
-
+from .locators import BasePageLocators
 
 
 class BasePage():
@@ -13,29 +12,12 @@ class BasePage():
         self.url = url
         self.browser.implicitly_wait(timeout)
 
-    def open(self):
-        self.browser.get(self.url)
-
     def is_element_present(self, how, what):
         try:
             self.browser.find_element(how, what)
         except NoElem:
             return False
         return True
-
-    def solve_quiz_and_get_code(self):
-        alert = self.browser.switch_to.alert
-        x = alert.text.split(" ")[2]
-        answer = str(math.log(abs((12 * math.sin(float(x))))))
-        alert.send_keys(answer)
-        alert.accept()
-        try:
-            alert = self.browser.switch_to.alert
-            alert_text = alert.text
-            print(f"Your code: {alert_text}")
-            alert.accept()
-        except NoAlertPresentException:
-            print("No second alert presented")
 
     def is_not_element_present(self, how, what, timeout=4):
         try:
@@ -56,7 +38,32 @@ class BasePage():
     def go_to_login_page(self):
         login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
         login_link.click()
-        #  return LoginPage(browser=self.browser, url=self.browser.current_url)
+
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
 
     def should_be_login_link(self):
         assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
+
+    def should_be_authorized_user(self):
+        assert self.is_element_present(*BasePageLocators.USER_ICON), "User icon is not presented," \
+                                                                     " probably unauthorised user"
+
+    def open(self):
+        self.browser.get(self.url)
+
+    def open_cart(self):
+        cart_button = self.browser.find_element(*BasePageLocators.CART)
+        cart_button.click()
+
